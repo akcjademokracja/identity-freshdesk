1e0c1007e1d7e12c854e7f10f3b35171de543d58
Marcin Koziej <marcin@akcjademokracja.pl>
2019-05-17 13:47:58 +0200

    mofe controller under proper module

---
+++ app/controllers/identity/freshdesk/freshdesk_controller.rb
@@ -0,0 +1,23 @@
+module Identity
+  module Freshdesk
+    class FreshdeskController < ActionController::Base
+      skip_before_action :verify_authenticity_token
+
+      def webhook
+        respond_to do |format|
+          format.json do
+            data = params[:freshdesk_webhook]
+            Rails.logger.info("FreshDesk webhook data #{params}")
+            raise 'JSON does not have :freshdesk_webhook key'
+
+            Identity::Freshdesk::FetchTicketWorker.perform_async(
+              data[:ticket][:id],
+              data[:triggered_event]
+            )
+            render json: { success: true }
+          end
+        end
+      end
+    end
+  end
+end
